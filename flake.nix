bbcdb840887e131d624b30457039c7ce560b0b1a
Break Yang <breakds@gmail.com>
2021-08-04 16:39:16 -0700

    Initialize the flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Phasic Policy Gradient";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/21.05";
+
+    utils.url = "github:numtide/flake-utils";
+    utils.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
+    "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin"
+  ] (system:
+    let pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [
+            # Use this overlay to provide customized python packages
+            # for development environment.
+            (import ./nix/overlays/dev.nix)
+          ];
+        };
+    in {
+      devShell = pkgs.callPackage ./nix/pkgs/dev-shell {};
+    });
+}
